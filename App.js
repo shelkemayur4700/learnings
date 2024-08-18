6e82d4d031522702b5c2e71ea94fb5e32ab0e784
Mayurs0 <mayurs@iconnectsolutions.com>
2024-08-18 08:53:16 +0530

    [LEARN]:Learned basics of react and how it works internally

--- App.js
+++ App.js
@@ -1,50 +1,76 @@
-// 1.
-{
-  /* <div id="parent">
-  <div id="child">
-    <h1>I' am h1 tag</h1>
-  </div>
-</div>; */
-}
-// 2.
-//   <div id="parent">
+import React from "react";
+import ReactDOM from "react-dom/client";
+// // 1.
+// {
+//   /* <div id="parent">
 //   <div id="child">
 //     <h1>I' am h1 tag</h1>
-//     <h2>I' am h1 tag</h2>
 //   </div>
-//   <div id="child2">
-//     <h1>I' am h1 tag</h1>
-//   </div>
-// </div>;
+// </div>; */
+// }
+// // 2.
+// //   <div id="parent">
+// //   <div id="child">
+// //     <h1>I' am h1 tag</h1>
+// //     <h2>I' am h1 tag</h2>
+// //   </div>
+// //   <div id="child2">
+// //     <h1>I' am h1 tag</h1>
+// //   </div>
+// // </div>;
 
-// 1. How we can create nested structure in react
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ])
-);
-// 2. How we can create nested structure in react
-const parent2 = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-  React.createElement(
-    "div",
-    { id: "child2" },
-    React.createElement("h1", {}, "I am an h1 tag")
-  ),
-]);
+// // 1. How we can create nested structure in react
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "Namste React"),
+//     React.createElement("h2", {}, "I am an h2 tag"),
+//   ])
+// );
+// // 2. How we can create nested structure in react
+// const parent2 = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "Namste react"),
+//     React.createElement("h2", {}, "I am an h2 tag"),
+//   ]),
+//   React.createElement(
+//     "div",
+//     { id: "child2" },
+//     React.createElement("h1", {}, "I am an h1 tag")
+//   ),
+// ]);
 
-//to reduce this complex code JSX is introduced
-console.log(parent); //returns object
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello world from react!"
-);
+// //to reduce this complex code JSX is introduced
+
+// console.log(parent); //returns object
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello world from react!"
+// );
+
+// React element ==> js object ==> html element render
+// const head1 = React.createElement("h1", { id: "heading" }, "Namste React");
+// console.log(head1);
+
+// LETS WRITE JSX
+// JSX => React elememt => js object => html element render
+// const head = <h1>Namste React using jsx</h1>;
+// console.log(head);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent2);
+
+// -----------LEARN COMPOENETS
+// normal jsx
+const headJSX = <h1>Jsx in js</h1>;
+const TitleComponent = () => <h1>Namaste React using JSX</h1>;
+
+// TITLE COMPOSITION
+// PUTTING COMPONENT INSIDE COMPONENT
+const HeadComponent = () => (
+  <div>
+    <TitleComponent />
+    <h1>React using functional component</h1>
+  </div>
+);
+root.render(<HeadComponent />);
